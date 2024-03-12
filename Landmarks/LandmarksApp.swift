58da54b6cb533248dbcf01848ed6586c9e8e3655
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2024-03-12 23:51:05 +0700

    ...

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 // App sử dụng SwiftUI app life cycle có structre tuân thủ theo App protocol.
 // Thuộc thính body có thể trả về 1 hoặc nhiều Scene, từ đó cung cấp nội dung để hiển thị.
 // Thuộc tính @main là điểm bắt đầu của ứng dụng.
